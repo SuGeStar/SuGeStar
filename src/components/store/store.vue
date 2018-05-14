6034c41794e3b3168d40b895355100eb6b8db1b5
wangduan123 <2949004924@qq.cpm>
2018-05-14 17:07:10 +0800

    wangduag

--- src/components/store/store.vue
+++ src/components/store/store.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="store">
-    <mt-tabbar fixed>
+    <header>
+      <div class="home-total">
+        <img class="home-search" src="../../assets/image/search.png">
+      <input class="home-input" type="" name="" id="" placeholder="搜索商品">
+      </div>
+      <img class="home-shopping" src="../../assets/image/shopping.png">
+        <img class="home-order" src="../../assets/image/order.png">
+    </header>
+    <mt-swipe class="swiper" :auto="4000">
+      <mt-swipe-item><img src="../../assets/image/banner01.png"></mt-swipe-item>
+      <mt-swipe-item><img src="../../assets/image/banner02.png"></mt-swipe-item>
+      <mt-swipe-item><img src="../../assets/image/banner01.png"></mt-swipe-item>
+    </mt-swipe>
+<swiper :options="swiperOption" ref="mySwiper" class="swiper1">
+   <swiper-slide>I'm Slide 1</swiper-slide>
+   <swiper-slide>I'm Slide 2</swiper-slide>
+</swiper>
+  <div class="swiper-pagination"  slot="pagination"></div>
+    <div class="swiper-button-prev" slot="button-prev"></div>
+    <div class="swiper-button-next" slot="button-next"></div>
+    <div class="swiper-scrollbar"   slot="scrollbar"></div>
+
+   <mt-tabbar fixed>
       <mt-tab-item id="home" href="#/">
         <i slot="icon" class="icon icon-star"></i>
         SG星球
@@ -16,3 +38,17 @@
     </mt-tabbar>
   </div>
 </template>
+<style lang="less" scoped>
+  @import '../../assets/less/store.less';
+</style>
+<script>
+
+export default {
+  created () {
+    return {
+      swiperOption: {
+      }
+    }
+  }
+}
+</script>
