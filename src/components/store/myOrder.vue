0566694caeec8ec5db56fb5f064323872de26ed1
unknown <>
2018-07-14 23:57:34 +0800

    2018070423

--- src/components/store/myOrder.vue
+++ src/components/store/myOrder.vue
@@ -9,28 +9,27 @@
     <div class="my-order-container">
       <!--所有订单-->
       <div class="each-order-list-container" v-for="(order,index) in orderList" :key="index">
-        <p class="shopName">{{order.product.brand}}
-          <span class="fr DFK">待付款</span>
+        <p class="shopName">{{order.product.store_name}}
+          <!--<span class="fr DFK">待付款</span>
           <span class="fr DFH">待发货</span>
           <span class="fr YFH">已发货</span>
           <span class="fr YWC">已完成</span>
-          <span class="fr TK">退款</span>
+          <span class="fr TK">退款</span>-->
         </p>
         <div class="each-order-list">
           <div class="eol-img fl">
-            <img ng-src="http://www.sgyxmall.com//Upload/goods/store_20/2018-05-01/5ae7dba4d3eb2.jpg" alt="" src="http://www.sgyxmall.com//Upload/goods/store_20/2018-05-01/5ae7dba4d3eb2.jpg">
+            <img :src="getImg(order.product.default_img)" alt="">
           </div>
           <div class="eol-info fl">
-            <p class="eol-nam">&nbsp;Spring Leaf绿芙绵羊油（添加维生素E和羊胎素和玫瑰精油）9327269000383</p>
-            <p class="eol-type" style="width: 4.2rem;height: .3rem;overflow: hidden">颜色：如图，尺寸：100克</p>
-            <p class="eol-enc">¥ 50.00 + 严选积分58.00 或</p>
-            <p class="eol-prices">¥ <span class="eol_price ng-binding">108.00</span></p>
+            <p class="eol-nam">&nbsp;{{order.product.goods_name}}</p>
+            <p class="eol-type" style="width: 4.2rem;height: .3rem;overflow: hidden">颜色：{{order.color}}，尺寸：{{order.spec.size}}</p>
+            <p class="eol-enc">星币 {{order.spec.gold}} + ¥ {{order.spec.cash}}</p>
           </div>
-          <span class="eol-totle">X 1</span>
+          <span class="eol-totle">X {{order.total_num}}</span>
         </div>
         <div class="myOrderInfo">
-          <p><span>订单编号：</span>123465798</p>
-          <p><span>下单时间：</span>20170415</p>
+          <p><span>订单编号：</span>{{order.sn}}</p>
+          <p><span>下单时间：</span>{{order.created_at}}</p>
           <p><span>支付合计：<i>10元+20积分</i></span></p>
           <p><span>支付方式：</span>微信支付</p>
           <p><span>快递单号：<input id="txt" value="12345678999" readonly></span><i @click="copyTxt()" class="copy">一键复制</i></p>
@@ -39,8 +38,8 @@
         <div class="myOrder_receive">
           <div class="or_left fl"></div>
           <div class="or_right fl">
-            <p><span>收货人：asdfas</span> <span class="fr" style="color: black">464646</span></p>
-            <p><span>收货地址：卡了数据库的和了阿拉山口付货款来说福克斯</span></p>
+            <p><span>收货人：{{order.address.name}}</span> <span class="fr" style="color: black">{{order.address.phone}}</span></p>
+            <p><span>收货地址：{{order.address.detail}}</span></p>
           </div>
         </div>
         <p class="applyTui">
@@ -244,6 +243,7 @@ export default {
       isTab: true,
       idx: 0,
       orderList: [],
+      way: 'orderAll',
       getImg (url) {
         return 'http://img.sugebei.com' + url
       }
@@ -261,7 +261,30 @@ export default {
     * 一键复制 copyTxt()
     * */
     changeTab (_idx) {
-      this.idx = _idx
+      this.idx = _idx;
+      switch (_idx) {
+        case 0:
+          this.way = 'orderAll';
+          break;
+        case 1:
+          this.way = 'waitpay';
+          break;
+        case 2:
+          this.way = 'waitreceive';
+          break;
+        case 3:
+          this.way = 'completed';
+          break;
+        case 4:
+          // this.way = 'refund';
+          break;
+        case 5:
+          this.way = 'refund';
+          break;
+        default:
+          return false;
+      }
+      this.getOrderData(this.way, 1)
     },
     cancelOrder () {},
     orderApply () {},
@@ -281,20 +304,28 @@ export default {
       } catch(err){
         // appService.a("复制失败，请手动复制");
       }*/
+    },
+    // 获取对应数据
+    /*
+    * 参数一： 对应接口,[orderAll,waitpay,waitreceive,completed,refund]
+    * 参数二：页数
+    * */
+    getOrderData (way, page) {
+      this.$http.get(url + way + '?token=' + token + '&page=' + page)
+        .then(res => {
+          console.log(res)
+          this.orderList = res.data.data;
+        })
+        .catch(err => {
+          console.log(err)
+        })
     }
   },
   mounted () {
   },
   created () {
     // 获得购买订单
-    this.$http.get(url + 'orderAll?token=' + token)
-      .then(res => {
-        console.log(res)
-        this.orderList = res.data.data;
-      })
-      .catch(err => {
-        console.log(err)
-      })
+    this.getOrderData('orderAll', 1)
   }
 }
 </script>
