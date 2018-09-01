e7714f6a67f22287820823c78cffb779b2969ba8
zhuzio <>
2018-09-01 09:44:37 +0800

    2018090109

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -38,7 +38,7 @@ export default {
               localStorage.setItem('userinfo', JSON.stringify(response.data.data.userinfo))
               localStorage.setItem('level', response.data.data.userinfo.level)
               localStorage.setItem('token', response.data.data.token)
-              window.location.href = ''
+              window.location.href = '/index'
               // this.$router.push('/index')
             } else {
               Toast({
